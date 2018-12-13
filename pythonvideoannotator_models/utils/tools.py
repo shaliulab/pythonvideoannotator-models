d0cb3586cc1b979262fb3a2e5d4f2ea24b0ccc75
Manuel Manso <manel_manso@hotmail.com>
2018-12-13 14:05:53 +0000

    Added small error check for window size, order and derivative

--- pythonvideoannotator_models/utils/tools.py
+++ pythonvideoannotator_models/utils/tools.py
@@ -2,18 +2,18 @@ import os, math, numpy as np, cv2
 from math import factorial
 
 def list_folders_in_path(path):
-	if not os.path.exists(path): return []
-	return sorted([os.path.join(path, d) for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))])
+    if not os.path.exists(path): return []
+    return sorted([os.path.join(path, d) for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))])
 
 def list_files_in_path(path):
-	if not os.path.exists(path): return []
-	return sorted([os.path.join(path, d) for d in os.listdir(path) if not os.path.isdir(os.path.join(path, d))])
+    if not os.path.exists(path): return []
+    return sorted([os.path.join(path, d) for d in os.listdir(path) if not os.path.isdir(os.path.join(path, d))])
 
 def points_angle(p1, p2): 
-	x1, y1 = p1
-	x2, y2 = p2
-	rads = math.atan2(y2-y1,x2-x1)
-	return rads
+    x1, y1 = p1
+    x2, y2 = p2
+    rads = math.atan2(y2-y1,x2-x1)
+    return rads
 
 def min_dist_angles(ang1, ang2):
     tmp  = max(ang1, ang2)
@@ -25,30 +25,33 @@ def min_dist_angles(ang1, ang2):
     return min(angle1, angle2, angle3)
 
 def lin_dist(p0, p1):
-	return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
+    return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
 
 
 def savitzky_golay(y, window_size, order=0, deriv=0, rate=1):
-	try:
-		window_size = np.abs(np.int(window_size))
-		order = np.abs(np.int(order))
-	except ValueError as msg:
-		raise ValueError("window_size and order have to be of type int")
-	if window_size % 2 != 1 or window_size < 1:
-		raise TypeError("window_size size must be a positive odd number")
-	if window_size < order + 2:
-		raise TypeError("window_size is too small for the polynomials order")
-	order_range = range(order+1)
-	half_window = (window_size -1) // 2
-	# precompute coefficients
-	b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
-	m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
-	# pad the signal at the extremes with
-	# values taken from the signal itself
-	firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
-	lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
-	y = np.concatenate((firstvals, y, lastvals))
-	return np.convolve( m[::-1], y, mode='valid')
+    try:
+        window_size = np.abs(np.int(window_size))
+        order = np.abs(np.int(order))
+    except ValueError as msg:
+        raise ValueError("window_size and order have to be of type int")
+    if (window_size % 2 != 1) or (window_size < 1):
+        raise TypeError("window_size size must be a positive odd number")
+    if window_size < order + 2:
+        raise TypeError("window_size is too small for the polynomials order")
+    if deriv>order:
+        raise TypeError("order is too small for rate")
+        
+    order_range = range(order+1)
+    half_window = (window_size -1) // 2
+    # precompute coefficients
+    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
+    m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
+    # pad the signal at the extremes with
+    # values taken from the signal itself
+    firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
+    lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
+    y = np.concatenate((firstvals, y, lastvals))
+    return np.convolve( m[::-1], y, mode='valid')
 
 
 
