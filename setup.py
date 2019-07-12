5f2fa384104addc2fff3d747414e37c6f0caca0f
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-07-12 13:51:32 +0100

    Update versioning system

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-import re
+import re, os
+
+PACKAGE_PATH = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(PACKAGE_PATH, 'pythonvideoannotator_models','__init__.py'), 'r') as fd:
+    content = fd.read()
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', content, re.MULTILINE).group(1)
 
 setup(
 	name='Python video annotator - models',
-	version=0.7,
+	version=version,
 	description="""""",
 	author=['Ricardo Ribeiro'],
 	author_email='ricardojvr@gmail.com',
